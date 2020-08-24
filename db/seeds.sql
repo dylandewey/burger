03629204962b08e9f61eb1acd6f3361c715df328
Dylan Dewey <dylandewey41@gmail.com>
2020-08-24 16:41:51 -0600

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Bacon Cheese');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Hawaiian');
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Bacon Blu', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Gypsy', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Pastrami', false);
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Hickory BBQ Bacon');
